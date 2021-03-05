4928be3caf03c63447f12168c9f3f585074f9455
DicuCorporationCode <dicuCorporation@gmail.com>
2021-03-05 12:44:33 -0600

    upload init

---
+++ Crypter/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEncriptar = New System.Windows.Forms.Button()
+        Me.btnDesencriptar = New System.Windows.Forms.Button()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.RutaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnEncriptar
+        '
+        Me.btnEncriptar.Location = New System.Drawing.Point(151, 212)
+        Me.btnEncriptar.Name = "btnEncriptar"
+        Me.btnEncriptar.Size = New System.Drawing.Size(211, 39)
+        Me.btnEncriptar.TabIndex = 0
+        Me.btnEncriptar.Text = "Encriptar"
+        Me.btnEncriptar.UseVisualStyleBackColor = True
+        '
+        'btnDesencriptar
+        '
+        Me.btnDesencriptar.Location = New System.Drawing.Point(151, 257)
+        Me.btnDesencriptar.Name = "btnDesencriptar"
+        Me.btnDesencriptar.Size = New System.Drawing.Size(211, 39)
+        Me.btnDesencriptar.TabIndex = 1
+        Me.btnDesencriptar.Text = "Desencriptar"
+        Me.btnDesencriptar.UseVisualStyleBackColor = True
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Location = New System.Drawing.Point(172, 145)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.Size = New System.Drawing.Size(189, 20)
+        Me.txtContraseña.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(99, 145)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Contraseña: "
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RutaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(494, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'RutaToolStripMenuItem
+        '
+        Me.RutaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirToolStripMenuItem})
+        Me.RutaToolStripMenuItem.Name = "RutaToolStripMenuItem"
+        Me.RutaToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.RutaToolStripMenuItem.Text = "Ruta"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 387)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.btnDesencriptar)
+        Me.Controls.Add(Me.btnEncriptar)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEncriptar As System.Windows.Forms.Button
+    Friend WithEvents btnDesencriptar As System.Windows.Forms.Button
+    Friend WithEvents txtContraseña As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents RutaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Crypter/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System
+Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Form1
+    Private Const sSecretKey As String = "hola"
+
+    Sub main()
+        Encriptar()
+    End Sub
+
+    Sub Encriptar(ByVal InputFileName As String, ByVal OutputFileName As String, ByVal skey As String)
+
+        Dim fsInput As New FileStream(InputFileName, FileMode.Open, FileAccess.Read)
+        Dim fsEncriptado As New FileStream(OutputFileName, FileMode.Create, FileAccess.Write)
+        Dim DES As New DESCryptoServiceProvider()
+
+        'Establecer la clave de desbloqueo para el algoritmo DES
+        'Se necesita una clave de 64 bits y IV para este provedor
+
+        DES.Key = ASCIIEncoding.ASCII.GetBytes(sSecretKey)
+
+        'Establecer el vetor de inicio
+        DES.IV = ASCIIEncoding.ASCII.GetBytes(skey)
+
+        'Crear cifrado DES a partir de la instancia
+        Dim desencriptador As ICryptoTransform = DES.CreateEncryptor()
+        'Crear una secuencia de cifrado que transforma la secuencia 
+        'En archivos mediante el cifrado DES
+
+        Dim crypto As New CryptoStream(fsEncriptado, desencriptador, CryptoStreamMode.Write)
+
+        'Lee el texto del archivo en la matriz de bytes
+        Dim byteArray(fsInput.Length - 1) As Byte
+        fsInput.Read(byteArray, 0, byteArray.Length)
+
+        'Escribir el cifrado DES
+        crypto.Write(byteArray, 0, byteArray.Length)
+        crypto.Close()
+
+    End Sub
+
+    Sub Desencriptar(ByVal sInputFile As String, ByVal sOutputFile As String, ByVal Skey As String)
+        Dim des As New DESCryptoServiceProvider()
+
+        des.Key = ASCIIEncoding.ASCII.GetBytes(Skey)
+
+        des.IV = ASCIIEncoding.ASCII.GetBytes(Skey)
+
+        Dim fsRead As New FileStream(sInputFile, FileMode.Open, FileAccess.Read)
+
+        Dim desenc As ICryptoTransform = des.CreateDecryptor()
+
+        Dim cryptosStream As New CryptoStream(fsRead, des, CryptoStreamMode.Read)
+
+        Dim fsDesenc As New StreamWriter(sOutputFile)
+        fsDesenc.Write(New StreamReader(cryptosStream).ReadToEnd)
+        fsDesenc.Flush()
+        fsDesenc.Close()
+
+    End Sub
+
+    Private Sub btnEncriptar_Click(sender As Object, e As EventArgs) Handles btnEncriptar.Click
+        Dim ruta As String
+
+        ruta = Path.Combine(Application.StartupPath, direccion)
+
+        Dim fichero As New IO.StreamWriter(ruta)
+        fichero.WriteLine(txtclave.txt)
+        fichero.Close()
+
+        main()
+    End Sub
+
+    Private Sub btnDesencriptar_Click(sender As Object, e As EventArgs) Handles btnDesencriptar.Click
+        Desencriptar("", "", sSecretKey)
+
+    End Sub
+
+    Dim direccion As String
+
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+        Dim openFileDialog As New OpenFileDialog()
+
+        openFileDialog.Multiselect = False
+        openFileDialog.RestoreDirectory = True
+        openFileDialog.Title = "Crypter"
+        Try
+            If openFileDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+                direccion = openFileDialog.FileName
+                btnDesencriptar.Enabled = True
+                btnEncriptar.Enabled = True
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " & ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnDesencriptar.Enabled = False
+        btnEncriptar.Enabled = False
+    End Sub
+End Class
